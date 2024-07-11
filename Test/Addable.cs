a6b1ee5617ec0952d54f35b67c1d62805bd0a1df
JonaAnders <kian.jona.ks@gmail.com>
2024-07-11 12:46:34 +0200

    Projektdateien hinzufügen.

---
+++ Test/Addable.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace OperatorTest
+{
+    internal class Addable<T> where T : IAdditionOperators<T, T, T>, IFormattable
+    {
+        T x;
+        public Addable(T a)
+        {
+            x = a;
+        }
+        public static Addable<T> operator +(Addable<T> a, Addable<T> b)
+        {
+            return new(a.x + b.x);
+        }
+
+        public override string ToString()
+        {
+            return x.ToString();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OperatorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Addable<int>(1);
+            var b = new Addable<int>(2);
+            Console.WriteLine(a + b);
+        }
+    }
+}
